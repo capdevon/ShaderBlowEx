5f025cc70ca816001720ee34b26d75762cd0fca3
Wyatt Gillette <jefferydeaver2010@gmail.com>
2021-11-06 10:00:31 +0100

    code refactoring - add read() write() methods

--- src/main/java/org/shaderblowex/filter/BetterVignette/BetterVignetteFilter.java
+++ src/main/java/org/shaderblowex/filter/BetterVignette/BetterVignetteFilter.java
@@ -1,145 +1,176 @@
 package org.shaderblowex.filter.BetterVignette;
 
+import java.io.IOException;
 
 import com.jme3.asset.AssetManager;
+import com.jme3.export.InputCapsule;
+import com.jme3.export.JmeExporter;
+import com.jme3.export.JmeImporter;
+import com.jme3.export.OutputCapsule;
 import com.jme3.material.Material;
+import com.jme3.math.FastMath;
 import com.jme3.post.Filter;
 import com.jme3.renderer.RenderManager;
 import com.jme3.renderer.ViewPort;
 
- 
 public class BetterVignetteFilter extends Filter {
- 
-	/** Default values */
-	private static final float DEFAULT_VIGNETTING_STRENGTH = 0.5f;
-	private static final float DEFAULT_VIGNETTING_EXTENT = 50f;
-        private static final float DEFAULT_BLUR_SIZE = 0.1f;
-	private static final float DEFAULT_BLUR_QUALITY = 20f;
-        private static final float DEFAULT_GRAY_POWER = 2.0f;
-	private static final float DEFAULT_GRAY_MARGIN = 4f;
- 
-	 
-	private float vignetteStrength = DEFAULT_VIGNETTING_STRENGTH;
-	private float vignetteExtent = DEFAULT_VIGNETTING_EXTENT;
-	private float blurQuality=DEFAULT_BLUR_QUALITY;
-        private float blurSize =DEFAULT_BLUR_SIZE;
-        private float grayPower=DEFAULT_GRAY_POWER;
-        private float grayMargin =DEFAULT_GRAY_MARGIN;
-          
-        
-	/**
-	 * Default Constructor.
-	 */
-	public BetterVignetteFilter() {
-		 super("BetterVignetteFilter");
-	}
-        public BetterVignetteFilter(float vignetteStrength, float vignetteExtent,float blurQuality,float blurSize, float grayPower,float grayMargin  ) {
-            this();
-            this.vignetteStrength = vignetteStrength;
-            this.vignetteExtent = vignetteExtent;
-            this. blurQuality=blurQuality;
-            this.blurSize =blurSize;
-            this.grayPower=grayPower;
-            this.grayMargin =grayPower;
-	}
-	@Override
-	protected Material getMaterial() {
-		return this.material;
-	}
- 
-	@Override
-	protected void initFilter(final AssetManager manager, final RenderManager renderManager, final ViewPort vp,
-			final int w, final int h) {
-		this.material = new Material(manager, "ShaderBlowEx/MatDefs/BetterVignette/BetterVignette.j3md");
-	        material.setFloat("VignetteStrength", vignetteStrength);
-                material.setFloat("VignetteExtent", vignetteExtent);
-		material.setFloat("BlurQuality", blurQuality);
-                material.setFloat("BlurSize", blurSize);
-                material.setFloat("GrayPower", grayPower);
-                material.setFloat("GrayMargin", grayMargin);
-	}
 
-	 
- public void setVignetteStrength(float vignetteStrength)
-         {
-        checkFloatArgument(vignetteStrength, 0, 1.0f, "VignetteStrength");
-          if (this.material != null) {
-                  this.material.setFloat("VignetteStrength", vignetteStrength );
-                  this.vignetteStrength = vignetteStrength;
-          }
-	}
+    /**
+     * Default values
+     */
+    private static final float DEFAULT_VIGNETTING_STRENGTH = 0.5f;
+    private static final float DEFAULT_VIGNETTING_EXTENT = 50f;
+    private static final float DEFAULT_BLUR_SIZE = 0.1f;
+    private static final float DEFAULT_BLUR_QUALITY = 20f;
+    private static final float DEFAULT_GRAY_POWER = 2.0f;
+    private static final float DEFAULT_GRAY_MARGIN = 4f;
 
-	public float getVignetteStrength() {
-		return vignetteStrength;
-	}
-  	 
- public void setVignetteExtent(float vignetteExtent) 
-        {
-        checkFloatArgument(vignetteExtent, 0, 100.0f, "VignetteExtent");
-        if (this.material != null) {
-                 this.material.setFloat("VignetteExtent", vignetteExtent );
-                this.vignetteExtent = vignetteExtent;
+    private float vignetteStrength = DEFAULT_VIGNETTING_STRENGTH;
+    private float vignetteExtent = DEFAULT_VIGNETTING_EXTENT;
+    private float blurQuality = DEFAULT_BLUR_QUALITY;
+    private float blurSize = DEFAULT_BLUR_SIZE;
+    private float grayPower = DEFAULT_GRAY_POWER;
+    private float grayMargin = DEFAULT_GRAY_MARGIN;
+
+    /**
+     * Default Constructor.
+     */
+    public BetterVignetteFilter() {
+        super("BetterVignetteFilter");
+    }
+
+    public BetterVignetteFilter(float vignetteStrength, float vignetteExtent, float blurQuality, float blurSize, float grayPower, float grayMargin) {
+        this();
+        this.vignetteStrength = vignetteStrength;
+        this.vignetteExtent = vignetteExtent;
+        this.blurQuality = blurQuality;
+        this.blurSize = blurSize;
+        this.grayPower = grayPower;
+        this.grayMargin = grayPower;
+    }
+
+    @Override
+    protected Material getMaterial() {
+        return this.material;
+    }
+
+    @Override
+    protected void initFilter(AssetManager manager, RenderManager renderManager, ViewPort vp, int w, int h) {
+        material = new Material(manager, "ShaderBlowEx/MatDefs/BetterVignette/BetterVignette.j3md");
+        material.setFloat("VignetteStrength", vignetteStrength);
+        material.setFloat("VignetteExtent", vignetteExtent);
+        material.setFloat("BlurQuality", blurQuality);
+        material.setFloat("BlurSize", blurSize);
+        material.setFloat("GrayPower", grayPower);
+        material.setFloat("GrayMargin", grayMargin);
+    }
+
+    public void setVignetteStrength(float vignetteStrength) {
+        //checkFloatArgument(vignetteStrength, 0, 1.0f, "VignetteStrength");
+        vignetteStrength = FastMath.clamp(vignetteStrength, 0, 1.0f);
+        this.vignetteStrength = vignetteStrength;
+        if (material != null) {
+            material.setFloat("VignetteStrength", vignetteStrength);
         }
-	}
+    }
 
- public float getVignetteExtent() {
-		return vignetteExtent;
-	}      
-      	 
- public void setBlurSize(float blurSize) 
-        {
-        checkFloatArgument(blurSize, 0, 1.0f, "BlurSize");
-        if (this.material != null) {
-                 this.material.setFloat("BlurSize", blurSize );
-                this.blurSize = blurSize;
+    public float getVignetteStrength() {
+        return vignetteStrength;
+    }
+
+    public void setVignetteExtent(float vignetteExtent) {
+        //checkFloatArgument(vignetteExtent, 0, 100.0f, "VignetteExtent");
+        vignetteExtent = FastMath.clamp(vignetteExtent, 0, 100.0f);
+        this.vignetteExtent = vignetteExtent;
+        if (material != null) {
+            material.setFloat("VignetteExtent", vignetteExtent);
         }
-	}
+    }
 
- public float getBlurSize() {
-		return blurSize;
-	}      
-  public void setBlurQuality(float blurQuality) 
-        {
-        checkFloatArgument(blurQuality, 0, 50.0f, "BlurQuality");
-        if (this.material != null) {
-                 this.material.setFloat("BlurQuality", blurQuality );
-                this.blurQuality = blurQuality;
+    public float getVignetteExtent() {
+        return vignetteExtent;
+    }
+
+    public void setBlurSize(float blurSize) {
+        //checkFloatArgument(blurSize, 0, 1.0f, "BlurSize");
+        blurSize = FastMath.clamp(blurSize, 0, 1.0f);
+        this.blurSize = blurSize;
+        if (material != null) {
+            material.setFloat("BlurSize", blurSize);
         }
-	}
+    }
 
- public float getBlurQuality() {
-		return blurQuality;
-	} 
- 
- public void setGrayPower(float grayPower) 
-        {
-        checkFloatArgument(grayPower, 0, 5.0f, "GrayPower");
-        if (this.material != null) {
-                 this.material.setFloat("GrayPower", grayPower );
-                this.grayPower = grayPower;
+    public float getBlurSize() {
+        return blurSize;
+    }
+
+    public void setBlurQuality(float blurQuality) {
+        //checkFloatArgument(blurQuality, 0, 50.0f, "BlurQuality");
+        blurQuality = FastMath.clamp(blurQuality, 0, 50.0f);
+        this.blurQuality = blurQuality;
+        if (material != null) {
+            material.setFloat("BlurQuality", blurQuality);
         }
-	}
+    }
 
- public float getGrayPower() {
-		return grayPower;
-	} 
- 
-  public void setGrayMargin(float grayMargin) 
-        {
-        checkFloatArgument(grayMargin, 0, 10.0f, "GrayMargin");
-        if (this.material != null) {
-                 this.material.setFloat("GrayMargin", grayMargin );
-                this.grayMargin = grayMargin;
+    public float getBlurQuality() {
+        return blurQuality;
+    }
+
+    public void setGrayPower(float grayPower) {
+        //checkFloatArgument(grayPower, 0, 5.0f, "GrayPower");
+        grayPower = FastMath.clamp(grayPower, 0, 5.0f);
+        this.grayPower = grayPower;
+        if (material != null) {
+            material.setFloat("GrayPower", grayPower);
         }
-	}
+    }
 
- public float getGrayMargin() {
-		return grayMargin;
-	} 
- 
-    private   void checkFloatArgument(float value, float min, float max, String name) {
-if (value < min || value > max) {
-throw new IllegalArgumentException(name + " was " + value + " but should be between " + min + " and " + max);
-}
-}
+    public float getGrayPower() {
+        return grayPower;
+    }
+
+    public void setGrayMargin(float grayMargin) {
+        //checkFloatArgument(grayMargin, 0, 10.0f, "GrayMargin");
+        grayMargin = FastMath.clamp(grayMargin, 0, 10f);
+        this.grayMargin = grayMargin;
+        if (material != null) {
+            material.setFloat("GrayMargin", grayMargin);
+        }
+    }
+
+    public float getGrayMargin() {
+        return grayMargin;
+    }
+
+    @Override
+    public void write(JmeExporter ex) throws IOException {
+        super.write(ex);
+        OutputCapsule oc = ex.getCapsule(this);
+        oc.write(vignetteStrength, "vignetteStrength", DEFAULT_VIGNETTING_STRENGTH);
+        oc.write(vignetteExtent, "vignetteExtent", DEFAULT_VIGNETTING_EXTENT);
+        oc.write(blurQuality, "blurQuality", DEFAULT_BLUR_QUALITY);
+        oc.write(blurSize, "blurSize", DEFAULT_BLUR_SIZE);
+        oc.write(grayPower, "grayPower", DEFAULT_GRAY_POWER);
+        oc.write(grayMargin, "grayMargin", DEFAULT_GRAY_MARGIN);
+    }
+
+    @Override
+    public void read(JmeImporter im) throws IOException {
+        super.read(im);
+        InputCapsule ic = im.getCapsule(this);
+        vignetteStrength = ic.readFloat("vignetteStrength", DEFAULT_VIGNETTING_STRENGTH);
+        vignetteExtent = ic.readFloat("vignetteExtent", DEFAULT_VIGNETTING_EXTENT);
+        blurQuality = ic.readFloat("blurQuality", DEFAULT_BLUR_QUALITY);
+        blurSize = ic.readFloat("blurSize", DEFAULT_BLUR_SIZE);
+        grayPower = ic.readFloat("grayPower", DEFAULT_GRAY_POWER);
+        grayMargin = ic.readFloat("grayMargin", DEFAULT_GRAY_MARGIN);
+    }
+
+    @Deprecated
+    private void checkFloatArgument(float value, float min, float max, String name) {
+        if (value < min || value > max) {
+            throw new IllegalArgumentException(name + " was " + value + " but should be between " + min + " and " + max);
+        }
+    }
 }
