7a3f358cee951c4f9764c00149868ed97dec1e03
Wyatt Gillette <jefferydeaver2010@gmail.com>
2021-11-28 17:27:02 +0100

    correct typos

--- src/main/java/org/shaderblowex/filter/RadialHaloGlowFilter.java
+++ src/main/java/org/shaderblowex/filter/RadialHaloGlowFilter.java
@@ -6,6 +6,7 @@ import com.jme3.export.JmeExporter;
 import com.jme3.export.JmeImporter;
 import com.jme3.export.OutputCapsule;
 import com.jme3.material.Material;
+import com.jme3.math.FastMath;
 import com.jme3.post.Filter;
 import com.jme3.renderer.RenderManager;
 import com.jme3.renderer.ViewPort;
@@ -43,8 +44,8 @@ public class RadialHaloGlowFilter extends Filter {
         return strength;
     }
 
-    public void setStrength(float newStrength) {
-        strength = checkFloatArgument(newStrength, 0.0f, 100.0f);
+    public void setStrength(float strength) {
+        this.strength = FastMath.clamp(strength, 0.0f, 100.0f);
         if (material != null) {
             material.setFloat("Strength", strength);
         }
@@ -54,20 +55,10 @@ public class RadialHaloGlowFilter extends Filter {
         return brightness;
     }
 
-    public void setBrightness(float newStrength) {
-        brightness = checkFloatArgument(newStrength, 0.0f, 1.0f);
+    public void setBrightness(float brightness) {
+        this.brightness = FastMath.clamp(brightness, 0.0f, 1.0f);
         if (material != null) {
             material.setFloat("Brightness", brightness);
-        }
-    }
-
-    private float checkFloatArgument(float value, float min, float max) {
-        if (value < min) {
-            return min;
-        } else if (value > max) {
-            return max;
-        } else {
-            return value;
         }
     }
 
