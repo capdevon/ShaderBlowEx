9e98ff30f2cc8052616d689c5c648aeed470e44d
Wyatt Gillette <jefferydeaver2010@gmail.com>
2021-11-29 08:41:40 +0100

    Update BleachFilter.java

--- src/main/java/org/shaderblowex/filter/BleachFilter.java
+++ src/main/java/org/shaderblowex/filter/BleachFilter.java
@@ -6,6 +6,7 @@ import com.jme3.export.JmeExporter;
 import com.jme3.export.JmeImporter;
 import com.jme3.export.OutputCapsule;
 import com.jme3.material.Material;
+import com.jme3.math.FastMath;
 import com.jme3.post.Filter;
 import com.jme3.renderer.RenderManager;
 import com.jme3.renderer.ViewPort;
@@ -29,15 +30,13 @@ public class BleachFilter extends Filter {
      */
     public BleachFilter(float strength) {
         this();
-        checkFloatArgument(strength, 0f, 5f, "Strength");
-        this.strength = strength;
+        this.strength = FastMath.clamp(strength, 0f, 5f);
     }
 
     @Override
     protected void initFilter(AssetManager manager, RenderManager renderManager, ViewPort vp, int w, int h) {
         material = new Material(manager, "ShaderBlowEx/MatDefs/Bleach/Bleach.j3md");
         material.setFloat("Strength", strength);
-
     }
 
     @Override
@@ -51,11 +50,10 @@ public class BleachFilter extends Filter {
      * @param The strength of the effect.
      */
     public void setStrength(float strength) {
-        checkFloatArgument(strength, 0, 5f, "Strength");
+        this.strength = FastMath.clamp(strength, 0f, 5f);
         if (material != null) {
             material.setFloat("Strength", strength);
         }
-        this.strength = strength;
     }
 
     /**
@@ -78,13 +76,7 @@ public class BleachFilter extends Filter {
     public void read(JmeImporter im) throws IOException {
         super.read(im);
         InputCapsule ic = im.getCapsule(this);
-        strength = (Float) ic.readFloat("strength", DEFAULT_STRENGTH);
-    }
-
-    private void checkFloatArgument(float value, float min, float max, String name) {
-        if (value < min || value > max) {
-            throw new IllegalArgumentException(name + " was " + value + " but should be between " + min + " and " + max);
-        }
+        strength = ic.readFloat("strength", DEFAULT_STRENGTH);
     }
 
 }
